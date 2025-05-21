5da2231ab39fa780a79165e79fb2c766903bbb56
kimjunsu <junsu4621@naver.com>
2025-05-21 12:41:23 +0900

    수정1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const page = () => {
     return (
-        <div className="flex justify-center items-center bg-amber-300 ">
-            CI/CD Test를 위한 임시 프로젝트입니다.
+        <div>
+            <div className="flex justify-center items-center bg-amber-300 ">
+                CI/CD Test를 위한 임시 프로젝트입니다.
+            </div>
+            <div>2025-05-21 12시41분 수정</div>
         </div>
     );
 };
