42f60de34eb1d647e9d99fdfe8dd5366e8ef8fa7
kimjunsu <junsu4621@naver.com>
2025-05-21 12:10:29 +0900

    임시 테스트

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+    return (
+        <div className="flex justify-center items-center bg-amber-300 ">
+            CI/CD Test를 위한 임시 프로젝트입니다.
+        </div>
+    );
+};
+
+export default page;
